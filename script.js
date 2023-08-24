9c7b59d89240ae98a6d32b6a2b2c6e36c7ff8d8b
Floatingtales <dillan.martin@ymail.com>
2023-08-24 20:09:36 +0800

    more changes

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ var randomNumberGenerator = function () {
   return randomInteger;
 };
 
+var randomNumberGenerator = function () {
+  var randomInteger = Math.floor(Math.random() * 3);
+  return randomInteger;
+};
+
+var randomNumberGenerator = function () {
+  var randomInteger = Math.floor(Math.random() * 3);
+  return randomInteger;
+};
+
 var main = function (input) {
   var myOutputValue = randomNumberGenerator();
   console.log(`My output value is ${myOutputValue}`);
